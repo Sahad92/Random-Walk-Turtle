a2cf07257c4064d39ab2d0057ad40b8172f75701
Sahad Jakhavala <sahadjak92@gmail.com>
2023-06-02 10:07:15 +0530

    a

---
+++ day18a.py
@@ -0,0 +1,26 @@
+# random walk turtle in python programming
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r,g,b)
+    return random_color
+
+
+directions = [0,90,180,270]
+tim.pensize(10)
+tim.speed("fastest")
+
+
+for _ in range(100):
+    
+    tim.color(random_color())
+    tim.forward(25)
+    tim.setheading(random.choice(directions))
